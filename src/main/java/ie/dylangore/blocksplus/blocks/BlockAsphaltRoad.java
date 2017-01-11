3a7b49e9bf86cab30ed884c854b9565bc57ee9e4
Dylan Gore <hello@dylangore.ie>
2017-01-11 15:41:29 +0000

    Added block: asphalt_road

---
+++ src/main/java/ie/dylangore/blocksplus/blocks/BlockAsphaltRoad.java
@@ -0,0 +1,39 @@
+package ie.dylangore.blocksplus.blocks;
+
+import ie.dylangore.blocksplus.Reference;
+import ie.dylangore.blocksplus.blocks.base.BlockBase;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import scala.tools.nsc.backend.icode.TypeKinds;
+
+/**
+ * Project: BlocksPlus
+ * File: BlockAsphaltRoad
+ * Author: Dylan Gore (hello@dylangore.ie)
+ * Date Created: 08 January 2017
+ */
+public class BlockAsphaltRoad extends BlockBase{
+    public BlockAsphaltRoad(Material material, String name) {
+        super(material, name);
+
+    }
+
+    @Override
+    public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn) {
+
+        if (entityIn instanceof Entity) {
+            if (((EntityLivingBase) entityIn).getActivePotionEffect(Potion.getPotionById(Reference.PotionEffects.SPEED.getPotionId())) == null) {
+                ((EntityLivingBase) entityIn).addPotionEffect(new PotionEffect(Potion.getPotionById(Reference.PotionEffects.SPEED.getPotionId()), 20, 2));
+            }
+            if (((EntityLivingBase) entityIn).getActivePotionEffect(Potion.getPotionById(Reference.PotionEffects.JUMP.getPotionId())) == null) {
+                ((EntityLivingBase) entityIn).addPotionEffect(new PotionEffect(Potion.getPotionById(Reference.PotionEffects.JUMP.getPotionId())));
+            }
+        }
+        super.onEntityWalk(worldIn, pos, entityIn);
+    }
+}
--- src/main/java/ie/dylangore/blocksplus/init/ModBlocks.java
+++ src/main/java/ie/dylangore/blocksplus/init/ModBlocks.java
@@ -1,7 +1,12 @@
 package ie.dylangore.blocksplus.init;
 
+import ie.dylangore.blocksplus.BlocksPlus;
+import ie.dylangore.blocksplus.Reference;
+import ie.dylangore.blocksplus.blocks.BlockAsphaltRoad;
+import ie.dylangore.blocksplus.blocks.base.BlockBase;
 import ie.dylangore.blocksplus.util.LogHelper;
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -14,14 +19,22 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModBlocks {
 
+    public static BlockAsphaltRoad blockAsphaltRoad;
+
     public static void init(){
+
+        blockAsphaltRoad = new BlockAsphaltRoad(Material.ROCK, Reference.BlocksPlusBlocks.ASPHALT_ROAD.getName());
+        registerSimpleItemBlock(blockAsphaltRoad, Reference.BlocksPlusBlocks.ASPHALT_ROAD.getName());
+
         LogHelper.info("Blocks registered!");
     }
 
-    public static void registerBlock(ItemBlock itemBlock, Block block, String registryName){
-        itemBlock = new ItemBlock(block);
+    public static void registerSimpleItemBlock(Block block, String registryName){
+        ItemBlock itemBlock = new ItemBlock(block);
         itemBlock.setRegistryName(registryName);
         GameRegistry.register(itemBlock);
         GameRegistry.register(block);
+
+        BlocksPlus.proxy.registerItemRenderer(itemBlock, 0, registryName);
     }
 }
