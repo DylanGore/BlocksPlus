8d77d89bdc6c5ae7c51336b98df11bc55a922e30
Dylan Gore <hello@dylangore.ie>
2017-01-20 16:24:05 +0000

    Added better automatic version replacement

--- src/main/java/ie/dylangore/blocksplus/Reference.java
+++ src/main/java/ie/dylangore/blocksplus/Reference.java
@@ -15,7 +15,7 @@ public class Reference {
 
     public static final String ID = "blocksplus";
     public static final String NAME = "Blocks Plus";
-    public static final String VERSION = "3.0.0";
+    public static final String VERSION = "mod_version";
     public static final String UPDATE_JSON_URL = "https://dylangore.ie/mods/blocksplus/updates.json"; //Temporary link
 
     public static final String CLIENT_PROXY_LOC = "ie.dylangore.blocksplus.proxy.ClientProxy";
